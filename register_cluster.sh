535f4730bafb5d9f6e6360ace0262dacbe9c34ba
adityakavalur <33232772+adityakavalur@users.noreply.github.com>
2020-09-28 14:44:11 -0400

    changed cluster name

--- register_cluster.sh
+++ register_cluster.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker exec slurmctld bash -c "/usr/bin/sacctmgr --immediate add cluster name=linux" && \
+docker exec slurmctld bash -c "/usr/bin/sacctmgr --immediate add cluster name=mycluster" && \
 docker-compose restart slurmdbd slurmctld
