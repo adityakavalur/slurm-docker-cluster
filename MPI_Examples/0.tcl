d38c3559def4bc1fbd4fb154f720685de46778ad
Aditya Kavalur <aditya@DOE7614168.local>
2020-10-01 23:25:29 -0400

    Added environment module file

---
+++ MPI_Examples/0.tcl
@@ -0,0 +1,7 @@
+#%Module1.0#####################################################################
+##
+## modules modulefile
+##
+module-whatis hello-world executable is loaded
+
+prepend-path PATH /data/MPI_Examples
---
+++ envmod.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+#Install TCL
+docker exec slurmctld bash -c \
+	' \
+	export Prefix=/data/tcl/8.6.10 && \
+	wget https://prdownloads.sourceforge.net/tcl/tcl8.6.10-src.tar.gz -P /data/ && \
+	tar -xzf /data/tcl8.6.10-src.tar.gz -C /data/ && \
+	rm -rf /data/tcl8.6.10-src.tar.gz && \
+	cd /data/tcl8.6.10/unix && \
+	./configure --prefix=$Prefix && \
+	make && \
+	make install && \
+	cd $Prefix/../ && \
+	ln -s 8.6.10 tcl && \
+	rm -rf /data/tcl8.6.10 \
+	'
+
+#Install environment module
+docker exec slurmctld bash -c \
+	' \
+	git clone --depth 1 --branch v4.6.0 https://github.com/cea-hpc/modules.git && \
+	cd modules && \
+	export Prefix=/data/envmod/4.6.0 && \
+	export Modulefiles=/data/modulefiles && \
+	./configure --prefix=$Prefix --modulefilesdir=$Modulefiles && \
+	make && \
+	make install && \
+	cd $Prefix/../ && \
+	ln -s 4.6.0 envmod && \
+	rm -rf /data/modules && \
+	cd /etc/profile.d/ && \
+	ln -s $Prefix/init/profile.sh modules.sh && \
+	ln -s $Prefix/init/profile.csh modules.csh \
+	'
